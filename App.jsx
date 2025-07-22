4dfc1acad24600a35328d4934e18de02cc8e6ba6
EmilieSJKL612 <151094902+EmilieSJKL612@users.noreply.github.com>
2025-07-21 19:32:51 -0600

    Completed App.jsx
    Upload the completed App.jsx based on FS GH RQs (RQ9-12)

--- App.jsx
+++ App.jsx
@@ -2,13 +2,35 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+// RQ9
+import { useState } from "react";
+import { tvShows } from "./shows/data.js";
+import ShowDetails from "./shows/ShowDetails.jsx";
+import ShowSelection from "./shows/ShowSelection.jsx";
+
+
 export default function App() {
+
+  // RQ9
+  const [shows] = useState(tvShows);
+
+  // RQ10
+  const [selectedShow, setSelectedShow] = useState(null);
+
+
+
   return (
     <>
       <header>
         <p>React TV</p>
+        {/* RQ11, 12 */}
+        <ShowSelection shows={shows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        {/* RQ11, 12 */}
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
